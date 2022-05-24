881624a8321e779a31a18c87a9576ef8829f5284
Steve Larsen <slarsen-3@att.net>
2022-05-24 18:19:52 -0500

    setup handlebars engine and views location

--- src/app.js
+++ src/app.js
@@ -2,11 +2,39 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicPath = path.join(__dirname, '../public');
 
+// Define paths fro Express config
+const publicPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicPath));
 
-app.get('/help', (req, res) => {});
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Steve Larsen',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Steve Larsen',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Steve Larsen',
+    message: 'This is a help message',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
