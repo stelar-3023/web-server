8ecabc6cd455f34017e2f1ef40f1827ba916b6f4
Steve Larsen <slarsen-3@att.net>
2022-05-28 17:41:10 -0500

    set up query string for /weather & address

--- src/app.js
+++ src/app.js
@@ -40,10 +40,28 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address',
+    });
+  }
+  console.log(req.query.address);
   res.send({
+    forecast: 'Overcast & 72 degrees',
     location: 'Nashville',
-    forecast: 'clear',
-    temperature: '72',
+    address: req.query.address,
+  });
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term',
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
   });
 });
 
