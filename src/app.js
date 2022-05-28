170d7225065272096e881f471aa82032fc54ae49
Steve Larsen <slarsen-3@att.net>
2022-05-28 18:39:26 -0500

    built json http endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
@@ -45,12 +47,27 @@ app.get('/weather', (req, res) => {
       error: 'You must provide an address',
     });
   }
-  console.log(req.query.address);
-  res.send({
-    forecast: 'Overcast & 72 degrees',
-    location: 'Nashville',
-    address: req.query.address,
-  });
+  geocode(req.query.address, (error, {latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
+  })
+  // console.log(req.query.address);
+  // res.send({
+  //   forecast: 'Overcast & 72 degrees',
+  //   location: 'Nashville',
+  //   address: req.query.address,
+  // });
 });
 
 app.get('/products', (req, res) => {
