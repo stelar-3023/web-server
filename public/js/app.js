f042f2198d0e0c90eda4868506f0aa2af1d76c69
Steve Larsen <slarsen-3@att.net>
2022-05-31 16:56:17 -0500

    wire up the user interface to display fetched data

--- public/js/app.js
+++ public/js/app.js
@@ -6,13 +6,43 @@
 //     });
 // });
 
-fetch('http://localhost:3000/weather?address=Cape%20Hatteras').then((response) => {
+// fetch('http://localhost:3000/weather?address=Cape%20Hatteras').then(
+//   (response) => {
+//     response.json().then((data) => {
+//       if (data.error) {
+//         console.log(data.error);
+//       } else {
+//         console.log(data.location);
+//         console.log(data.forecast);
+//       }
+//     });
+//   }
+// );
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
+      if (data.error) {
+        // console.log(data.error);
+        messageOne.textContent = data.error;
+      } else {
+        // console.log(data.location);
+        // console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
     });
-})
+  });
+});
